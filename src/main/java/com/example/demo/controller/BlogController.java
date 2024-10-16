4f25fa7cc0e6c17d046b3c1aa6a60293a90e78d9
dhwa02 <donghwa0702@gmail.com>
2024-10-16 14:47:47 +0900

    6주차 블로그 게시판 (수정)

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -1,17 +1,21 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.example.demo.model.domain.Article;
 import com.example.demo.model.service.AddArticleRequest;
 import com.example.demo.model.service.BlogService;
+
 
 @Controller // 컨트롤러 어노테이션 명시
 
@@ -37,5 +41,22 @@ public class BlogController {
         // 아무 작업도 하지 않음
     }
 
+    @GetMapping("/article_edit/{id}") // 게시판 링크 지정
+    public String article_edit(Model model, @PathVariable Long id ){
+        Optional<Article> list = blogService.findById(id); // 선택한 게시판 글
+
+        if(list.isPresent()){
+            model.addAttribute("article", list.get()); // 존재하면 Article 객체를 모델에 추가
+        } else {
+            // 처리할 로직 추가 (예시 : 오류 페이지로 리다이렉트, 예외 처리 등)
+        }
+        return "article_edit"; // .HTML 연결
+    }
+
+    @PutMapping("/api/article_edit/{id}")
+    public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
+        blogService.update(id, request);
+        return "redirect:/article_list"; // 글 수정 이후 .html 연결
+    }
 }
 
